3ae0a01e28b8678276a5145fcbfdb497554acfa8
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-10-19 20:01:54 -0300

    Create storybook button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,14 +1,30 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Heading = styled('h1', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  fontSize: '$2xl',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return (
-    <div>
-      <Heading>Hello world</Heading>
-    </div>
-  )
-}
+export type ButtonProps = ComponentProps<typeof Button>
